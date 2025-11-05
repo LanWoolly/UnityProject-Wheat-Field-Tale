8c31205429120f9ef284eef3829affb95f829c0b
LanWoolly <2295926851@qq.com>
2025-11-05 20:39:29 +0800

    2025.11.5
    创建 InventoryManager 和 Item

--- Farm/Assets/Scripts/Inventory/Logic/InventoryManager.cs
+++ Farm/Assets/Scripts/Inventory/Logic/InventoryManager.cs
@@ -7,6 +7,12 @@ namespace Farm.Inventory
     public class InventoryManager : Singleton<InventoryManager>
     {
         public ItemDataList_SO itemDataList_SO;
+
+        /// <summary>
+        /// 通过ID返回物品信息
+        /// </summary>
+        /// <param name="ID">itemID</param>
+        /// <returns></returns>
         public ItemDetails GetItemDetails(int ID)
         {
             return itemDataList_SO.itemDetailsList.Find(i => i.itemID == ID);
